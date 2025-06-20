95bf811ee5711104fcf425f551298c2820773c8f
chengfushi <303623518@qq.com>
2025-06-20 13:56:09 +0800

    Initial commit

---
+++ src/main/java/com/chengfu/yunpictureapi/YunPictureApiApplication.java
@@ -0,0 +1,13 @@
+package com.chengfu.yunpictureapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunPictureApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YunPictureApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chengfu/yunpictureapi/YunPictureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chengfu.yunpictureapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YunPictureApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
