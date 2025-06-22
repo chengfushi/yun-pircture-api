d6d7ef29fc2c6c67d571395054c6634873607056
chengfushi <303623518@qq.com>
2025-06-22 18:32:47 +0800

    更新yml

--- src/main/java/com/chengfu/yunpictureapi/YunPictureApiApplication.java
+++ src/main/java/com/chengfu/yunpictureapi/YunPictureApiApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * @author chengfu
