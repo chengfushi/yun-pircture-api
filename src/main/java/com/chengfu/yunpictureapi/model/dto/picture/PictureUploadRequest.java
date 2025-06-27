057037ee6e22cff989970477eaed14c32202fde2
chengfushi <303623518@qq.com>
2025-06-27 17:42:14 +0800

    批量抓取图片

--- src/main/java/com/chengfu/yunpictureapi/model/dto/picture/PictureUploadRequest.java
+++ src/main/java/com/chengfu/yunpictureapi/model/dto/picture/PictureUploadRequest.java
@@ -18,5 +18,10 @@ public class PictureUploadRequest implements Serializable {
     private String fileUrl;
 
     private static final long serialVersionUID = 1L;
+    /**
+     * 图片名称
+     */
+    private String picName;
+
 }
 
--- src/main/java/com/chengfu/yunpictureapi/service/PictureService.java
+++ src/main/java/com/chengfu/yunpictureapi/service/PictureService.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.chengfu.yunpictureapi.model.dto.picture.PictureQueryRequest;
 import com.chengfu.yunpictureapi.model.dto.picture.PictureReviewRequest;
+import com.chengfu.yunpictureapi.model.dto.picture.PictureUploadByBatchRequest;
 import com.chengfu.yunpictureapi.model.dto.picture.PictureUploadRequest;
 import com.chengfu.yunpictureapi.model.entity.Picture;
 import com.baomidou.mybatisplus.extension.service.IService;
@@ -56,4 +57,17 @@ public interface PictureService extends IService<Picture> {
     boolean doPictureReview(PictureReviewRequest pictureReviewRequest, User loginUser);
 
     void fillReviewParams(Picture picture, User loginUser);
+
+    /**
+     * 批量抓取和创建图片
+     *
+     * @param pictureUploadByBatchRequest
+     * @param loginUser
+     * @return 成功创建的图片数
+     */
+    Integer uploadPictureByBatch(
+            PictureUploadByBatchRequest pictureUploadByBatchRequest,
+            User loginUser
+    );
+
 }
