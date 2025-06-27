95fe2e0b310ff82e82fac3c845b5fb807cdb38ed
chengfushi <303623518@qq.com>
2025-06-27 14:29:18 +0800

    更改标签

--- src/main/java/com/chengfu/yunpictureapi/controller/PictureController.java
+++ src/main/java/com/chengfu/yunpictureapi/controller/PictureController.java
@@ -204,7 +204,7 @@ public class PictureController {
     @GetMapping("/tag_category")
     public BaseResponse<PictureTagCategory> listPictureTagCategory() {
         PictureTagCategory pictureTagCategory = new PictureTagCategory();
-        List<String> tagList = Arrays.asList("热门", "搞笑", "生活", "高清", "艺术", "校园", "背景", "简历", "创意");
+        List<String> tagList = Arrays.asList("热门", "搞笑", "生活", "高清", "艺术", "校园", "背景", "简历", "创意","头像");
         List<String> categoryList = Arrays.asList("模板", "电商", "表情包", "素材", "海报");
         pictureTagCategory.setTagList(tagList);
         pictureTagCategory.setCategoryList(categoryList);
